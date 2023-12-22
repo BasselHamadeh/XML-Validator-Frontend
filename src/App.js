8905e04829bdfe918ffa617d5ded5bc1d98b9069
BasselHamadeh <basselhamadeh3@gmail.com>
2023-12-22 03:39:03 +0100

    heading style

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Heading">
+      <h1>XML Validator</h1>
+      <p className="Description">Laden Sie XML-Dateien hoch und führen Sie Validierungen durch.</p>
     </div>
   );
 }
