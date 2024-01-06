203ff00dd2897d18861c70b9d350ad6be368fe86
BasselHamadeh <basselhamadeh3@gmail.com>
2024-01-06 16:35:28 +0100

    user connection working

--- src/View/UsersView.jsx
+++ src/View/UsersView.jsx
@@ -136,7 +136,7 @@ const UserPage = () => {
           ),
         }}
       />
-      <TableContainer component={Paper} style={{ marginTop: '10px', marginLeft: '7px' }}>
+      <TableContainer component={Paper} style={{ marginTop: '10px', marginLeft: '8px' }}>
         <Table sx={{ minWidth: 700 }} aria-label="customized table">
           <TableHead>
             <TableRow>
